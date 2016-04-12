fdd11d965720b23dbaec02e5890df8720a0a1c9a
johanrovala <jr.johan.ro@gmail.com>
2016-04-12 15:05:40 +0200

    basic transition between sprites for clue buttons implemented.

--- Assets/StoreLevel/Scripts/StoreScript.cs
+++ Assets/StoreLevel/Scripts/StoreScript.cs
@@ -1,11 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class StoreScript : MonoBehaviour {
 
+	Button[] clueButton;
+	Texture2D transitionSprite;
+	SpriteRenderer bg;
+	Sprite test;
+
 	// Use this for initialization
 	void Start () {
-	
+		clueButton = GameObject.Find ("Canvas").GetComponentsInChildren<Button>();
+		transitionSprite = Instantiate (Resources.Load ("Watermelon")) as Texture2D;
+		Rect rec = new Rect (0, 0, transitionSprite.width, transitionSprite.height);
+		test = Sprite.Create (transitionSprite, rec, new Vector2(0,0), 1);
+		//bg.sprite = Sprite.Create (transitionSprite, rec, new Vector2(0, 0),1f);
+		print (Resources.Load ("Watermelon"));
+		print (transitionSprite);
 	}
 	
 	// Update is called once per frame
@@ -17,5 +29,11 @@ public class StoreScript : MonoBehaviour {
 		print ("button works");
 		Level level = new Level (19, "testLudde");
 		level.launchLevel ();	
+	}
+
+	public void clueButtonTransition(int i) {
+		Button button = clueButton [i];
+		button.image.sprite = test;
+		print (transitionSprite);
 	}
 }
