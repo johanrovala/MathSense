11e66372dd73677c25a4708c7a88b40743bf5d78
johanrovala <jr.johan.ro@gmail.com>
2016-05-11 11:18:32 +0200

    Updated the way that the canvases reflect on screensize. Everything within the main screen, main levels and shop should now react naturally to the given screen size.

---
+++ Assets/Shop/Scripts/Coin.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coin{
+
+	public static int coinScore;
+
+	public static void updateCoinScore(string s, int n) {
+		if(s.Equals("+")){
+			coinScore += n;	
+		}else if(s.Equals("-")){
+			coinScore -= n;
+		}
+
+		// make a third condition in case something crazy happens
+	
+	}
+		
+	public static int getCoinScore() {
+		return coinScore;
+	}
+
+}
---
+++ Assets/Shop/Scripts/Level.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level{
+
+	private int reward;
+	private string path;
+
+	public Level (int reward, string path){
+		this.reward = reward;
+		this.path = path;
+	}
+
+	public int getCost(){
+		return this.reward;
+	}
+
+	public string getPath(){
+		return this.path;
+	}
+
+	public void launchLevel(){
+		Application.LoadLevel (path);
+	}
+	
+
+
+
+	
+
+}
